1ce262f258b03f93508531bd751cbb49ca5b697a
prashamsa9928 <prashamsakm@gmail.com>
2025-08-02 11:19:22 +0530

    ArithmeticOperators.java

--- ArithmeticOperators.java
+++ ArithmeticOperators.java
@@ -6,9 +6,9 @@ public class ArithmeticOperators {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		  Scanner sc = new Scanner(System.in);
-	      System.out.println("Enter first number");
+	      System.out.println("Enter a");
 	      int a=sc.nextInt();
-	      System.out.println("Enter second number");
+	      System.out.println("Enter b");
 	      int b=sc.nextInt();
 	      System.out.println("Addition of a and b is:" + (a+b));
 	      System.out.println("Subtraction of a and b is:" + (a-b));
