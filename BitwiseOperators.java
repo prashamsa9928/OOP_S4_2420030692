2154898917a9f510b2c2a1915a589bcb1e14e44a
prashamsa9928 <prashamsakm@gmail.com>
2025-08-03 23:47:36 +0530

    BitwiseOperators.java

--- BitwiseOperators.java
+++ BitwiseOperators.java
@@ -6,20 +6,20 @@ public class BitwiseOperators {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-
-        // Input for bitwise and shift operations
-        System.out.print("Enter first number: ");
+        System.out.print("Enter a");
         int a = sc.nextInt();
-        System.out.print("Enter second number: ");
+        System.out.print("Enter b");
         int b = sc.nextInt();
-        System.out.println("\n--- Bitwise Operations ---");
-        System.out.println("a & b (AND) = " + (a & b));
-        System.out.println("a | b (OR) = " + (a | b));
-        System.out.println("a ^ b (XOR) = " + (a ^ b));
+        System.out.println("AND Operator = " + (a & b));
+        System.out.println("OR Operator = " + (a | b));
+        System.out.println("XOR Operator = " + (a ^ b));
+        System.out.println("COMPLEMENT Operator of a = " + (~a));
+	System.out.println("COMPLEMENT Operator of b = " + (~b));  
         
 
   }
 
 
 	}
+
 
