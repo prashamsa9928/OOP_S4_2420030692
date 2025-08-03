9d4f9962e323a2dc05a8391940bc8aea7a141bb5
prashamsa9928 <prashamsakm@gmail.com>
2025-08-04 00:01:25 +0530

    CompoundAssignment.java

--- CompoundAssignment.java
+++ CompoundAssignment.java
@@ -6,22 +6,15 @@ public class CompoundAssignment {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
-
-        System.out.println("Original num = " + num);
-        num+=5;
-        System.out.println(num);
-        num-=2;
-        System.out.println(num);
-        num*=3;
-        System.out.println(num);
-        num/=2;
-        System.out.println(num);
-        num%=4;
-        System.out.println(num);
-}
+		System.out.println("Enter a");
+        int a = sc.nextInt();
+        System.out.println("a+=5 is"+(a+=5));
+        System.out.println("a-=5 is"+(a-=5));
+        System.out.println("a*=5 is"+(a*=5));
+        System.out.println("a/=5 is"+(a/=5));
+        System.out.println("a%=5 is"+(a%=5));
 
 
 	}
+
 
