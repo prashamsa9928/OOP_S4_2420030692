665160e7b167e6e475c59ea653ddd95531a38740
prashamsa9928 <prashamsakm@gmail.com>
2025-08-04 00:36:21 +0530

    Grade.java

--- Grade.java
+++ Grade.java
@@ -6,23 +6,22 @@ public class Grade {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-	     System.out.println("Enter marks: ");
+	     System.out.println("Enter marks of student:");
 	     int marks = sc.nextInt();
 	     if (marks >= 90 && marks <= 100) 
-	    	 System.out.println("grade = A+");
+	    	 System.out.println("Grade = A");
 	      else if (marks >= 80) 
-	    	 System.out.println("grade = A");
+	    	 System.out.println("Grade = B");
 	      else if (marks >= 70) 
-	    	 System.out.println("grade = B");
+	    	 System.out.println("Grade = C");
 	      else if (marks >= 60) 
-	    	 System.out.println("grade = C");
+	    	 System.out.println("Grade = D");
 	      else if (marks >= 50) 
-	    	 System.out.println("grade = D");
-	      else if (marks >= 35) 
-	    	 System.out.println("grade = E");
+	    	 System.out.println("Grade = E");
 	      else  
-	    	 System.out.println("grade = F (Fail)");
+	    	 System.out.println("Grade = F");
 		}
 
 	}
+
 
