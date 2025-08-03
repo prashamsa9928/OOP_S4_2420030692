5409d4fedee6eaac4c8cefe5b0b53b4ab5105ddf
prashamsa9928 <prashamsakm@gmail.com>
2025-08-04 00:21:59 +0530

    TernaryOperator.java

--- TernaryOperator.java
+++ TernaryOperator.java
@@ -6,13 +6,14 @@ public class TernaryOperator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-	    System.out.println("Enter first number: ");
+	    System.out.println("Enter a");
 	    int a = sc.nextInt();
-	    System.out.println("Enter second number: ");
+	    System.out.println("Enter b");
 	    int b = sc.nextInt();
-	    int max = (a > b) ? a : b;
-	     System.out.println("Greater number is :" + max);
+	    int greater = (a > b) ? a : b;
+	     System.out.println("Greater variable:" + greater);
 	  }
 
 	}
+
 
