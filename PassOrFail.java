de284ca0fe5b06279739d3e9cf8cda9589c40001
prashamsa9928 <prashamsakm@gmail.com>
2025-08-03 23:31:39 +0530

    PassOrFail.java

--- PassOrFail.java
+++ PassOrFail.java
@@ -6,16 +6,16 @@ public class PassOrFail{
 	  public static void main(String[] args) {
 	    // TODO Auto-generated method stub
 	      Scanner sc = new Scanner(System.in);
-	          System.out.print("Did you pass theory (true/false)? ");
+	          System.out.println("Passed theory exam (true/false)? ");
 	          boolean theory = sc.nextBoolean();
-	          System.out.print("Did you pass practical (true/false)? ");
+	          System.out.println("Passed practical exam (true/false)? ");
 	          boolean practical = sc.nextBoolean();
-
-	          if (theory && practical)
-	              System.out.println("You passed both theory and practical.");
+                  if (theory && practical)
+	              System.out.println("Passed both the exams.");
 	          else
-	              System.out.println("You did not pass both exams.");
+	              System.out.println("Didn't pass both the exams.");
 	  }
 
 	}
+
 
