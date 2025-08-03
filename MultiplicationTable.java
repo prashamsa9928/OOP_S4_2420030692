eba67a6c8f7d88883580b84fd46a93b23de5b65e
prashamsa9928 <prashamsakm@gmail.com>
2025-08-04 01:14:10 +0530

    MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -6,13 +6,14 @@ public class MultiplicationTable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-	     System.out.println("Enter a number to print it's multiplication table:");
-	     int num =sc.nextInt();
+	     System.out.println("Enter a");
+	     int a =sc.nextInt();
 	     for(int i=1;i<=10;i++)
 	     {
-	    	 System.out.println(num + " x " + i + " = " + (num * i));
+	    	 System.out.println(a + " * " + i + " = " + (a * i));
 	     }
 
 	}
 
 }
+
