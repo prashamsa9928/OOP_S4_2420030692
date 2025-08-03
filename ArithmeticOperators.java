15c263302696dece5f6417ddee55812e6a749dd8
prashamsa9928 <prashamsakm@gmail.com>
2025-08-03 23:08:05 +0530

    Operators.java

--- ArithmeticOperators.java
+++ ArithmeticOperators.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 public class ArithmeticOperators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		  Scanner sc = new Scanner(System.in);
+	      // TODO Auto-generated method stub
+	      Scanner sc = new Scanner(System.in);
 	      System.out.println("Enter a");
 	      int a=sc.nextInt();
 	      System.out.println("Enter b");
 	      int b=sc.nextInt();
-	      System.out.println("Addition of a and b is:" + (a+b));
-	      System.out.println("Subtraction of a and b is:" + (a-b));
-	      System.out.println("Multiplication of a and b is:"+ (a*b));
-	      System.out.println("Division of a and b is:" + (a/b));
-	      System.out.println("Modulus of a and b is:" + (a%b));
+	      System.out.println("Sum is:"+(a+b));
+              System.out.println("Diff is:"+(a-b));
+              System.out.println("Product is:"+(a*b));
+              System.out.println("Quotient is:"+(a/b));
+              System.out.println("Modulus is:"+(a%b));
         
 	}
 
 }
+
