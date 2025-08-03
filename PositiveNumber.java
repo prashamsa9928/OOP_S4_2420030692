8eb779651b2b5550a8cb820545b798615de179d9
prashamsa9928 <prashamsakm@gmail.com>
2025-08-04 00:29:59 +0530

    PositiveNumber.java

--- PositiveNumber.java
+++ PositiveNumber.java
@@ -6,15 +6,16 @@ public class PositiveNumber {
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 	    Scanner sc = new Scanner(System.in);
-	    System.out.println("Enter a number: ");
+	    System.out.println("Enter a");
 	    int a = sc.nextInt();
 	    if(a > 0)
 	    	System.out.println("Number is positive");
 	    else if( a < 0)
 	    	System.out.println("Number is negative");
 	    else
-	    	System.out.println("Zero");
+	    	System.out.println("Number is Zero");
 		}
 
 	}
+
 
